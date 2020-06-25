57dece140c6c2ff3df063bf83d4f19c2801385d5
ohye3166 <562672808@qq.com>
2020-06-25 11:05:51 +0800

    本地工程添加commit

---
+++ src/main/java/first.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class first{
+/*abc你*/
+    public static int getResult(int a,int b){
+        if(a < b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int k;
+        while(b != 0){
+            k = a % b;
+            a = b;
+            b = k;
+        }
+        /*System.out.println("你好");*/
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int num = a * b / getResult(a,b);
+            System.out.println(num);
+        }
+    }
+}
