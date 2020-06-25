8eb5fc7870ee6c54262b0314a35d4d2bf6894734
ohye3166 <562672808@qq.com>
2020-06-25 11:55:12 +0800

    second

--- src/main/java/second.java
+++ src/main/java/second.java
@@ -2,5 +2,29 @@
  * @author chenzongyi
  * @create 2020-06-25-11:24
  */
+import java.util.*;
 public class second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            double n = sc.nextDouble();
+            System.out.printf("%.1f",function(n));
+        }
+    }
+
+    private static double function(double n) {
+        double low = 0.0;
+        double high = n;
+        double mid = n / 2;
+
+        while(high - mid >0.01){
+            if(mid * mid * mid > n) {
+                high = mid;
+            }else{
+                low = mid;
+            }
+            mid = (low + high) / 2;
+        }
+        return mid;
+    }
 }
